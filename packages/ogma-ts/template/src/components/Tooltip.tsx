db8e1b2cbc6d972a6b029e6542a76038830ed793
Ebernn <r1.bernier@orange.fr>
2023-02-27 14:30:51 +0100

    Fixed types

--- packages/ogma-ts/template/src/components/Tooltip.tsx
+++ packages/ogma-ts/template/src/components/Tooltip.tsx
@@ -1,13 +1,16 @@
-import React from "react";
-import { Point } from "@linkurious/ogma";
-import "./Tooltip.css";
+import React from 'react';
+import { Point } from '@linkurious/ogma';
+import './Tooltip.css';
 
 type TooltipProps<T extends Object> = {
   position: Point;
   data: T;
 };
 
-export default function Tooltip<T>({ position, data }: TooltipProps<T>) {
+export default function Tooltip<T extends Object>({
+  position,
+  data,
+}: TooltipProps<T>) {
   return (
     <div className="tooltip" style={{ top: position.y + 50, left: position.x }}>
       <div className="tooltip-content">
